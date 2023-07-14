7dca7a01c53574fd42ac600e29adb57bc1054cca
JuanDNJ <juandevnjv@gmail.com>
2023-07-14 05:26:42 +0200

    Add: Datos en la tabla pets

--- src/db/database.sql
+++ src/db/database.sql
@@ -38,17 +38,24 @@ DROP TABLE IF EXISTS pets;
 CREATE TABLE pets(
 	pet_id int not null auto_increment,
     name varchar(20) not null,
+    photo_url varchar(255) null,
+    years int(3) null default(null),
+    category char(20) not null,
 	create_at timestamp not null default (now()),
     primary key(pet_id)
 );
-INSERT INTO pets(name) 
+INSERT INTO pets(name,photo_url,years, category) 
 VALUES 
-("Winter"),
-("Sunn"),
-("Moonlight"),
-("Toby"),
-("Jinnx"),
-("Po");
+("Winter","https://fotografias.lasexta.com/clipping/cmsimages01/2022/08/09/3FFA8546-05CE-4608-9B69-6602D02A4C58/cachorro-pomsky_103.jpg?crop=1183,887,x0,y0&width=1200&height=900&optimize=low&format=webply",8,"dogs"),
+("Sunn","https://estaticos-cdn.prensaiberica.es/clip/823f515c-8143-4044-8f13-85ea1ef58f3a_16-9-discover-aspect-ratio_default_0.jpg",3,"dogs"),
+("Moonlight","https://images.unsplash.com/photo-1535591273668-578e31182c4f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjM2NTI5fQ",1,"fish"),
+("Toby","https://images.theconversation.com/files/525305/original/file-20230510-15-ol6krl.jpg?ixlib=rb-1.1.0&rect=0%2C552%2C4500%2C2250&q=45&auto=format&w=668&h=324&fit=crop",1,"rodents"),
+("Jinnx","https://c.files.bbci.co.uk/48DD/production/_107435681_perro1.jpg",2,"dogs"),
+("Jilger","https://www.thesprucepets.com/thmb/r23RBk0t4DC9UHp2pTzuXLz7Jj4=/3600x0/filters:no_upscale():strip_icc()/popular-small-bird-species-390926-hero-d3d0af7bb6ed4947b0c3c5afb4784456.jpg",0.5,"birds"),
+("Colorin","https://media.newyorker.com/photos/5a95a5b13d9089123c9fdb7e/1:1/w_3289,h_3289,c_limit/Petrusich-Dont-Mess-with-the-Birds.jpg",1,"birds"),
+("Boby","https://images.hola.com/imagenes/mascotas/20230428231045/razas-perros-toy/1-242-978/schnauzer-miniatura-m.jpg?tx=w_680",7,"dogs"),
+("Tobias","https://www.nationalgeographic.com.es/medio/2022/04/26/camaleon_4fcd8b91_800x800.jpg",10,"reptiles"),
+("Po","https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png",5,"cats");
 
 DROP TABLE IF EXISTS banners;
 
@@ -64,3 +71,21 @@ INSERT INTO banners(title, text, color)
 VALUES 
 ("Por que adoptar?","Facere at pariatur, repellendus nulla, neque dolorem accusamus debitis quasi nostrum nihil perferendis quae quisquam dolorum corporis cumque temporibus quaerat placeat rerum.","white"),
 ("Beneficios al adoptar","Facere at pariatur, repellendus nulla, neque dolorem accusamus debitis quasi nostrum nihil perferendis quae quisquam dolorum corporis cumque temporibus quaerat placeat rerum.","pink");
+
+
+DROP TABLE IF EXISTS front_pages;
+
+CREATE TABLE front_pages(
+	front_id int not null auto_increment,
+    title varchar(50) not null,
+    text varchar(255) not null,
+    photo_url varchar(255) null,
+    bg_color char(20) null,
+    text_color char(10) null,
+    title_color char(10) null,
+	create_at timestamp not null default (now()),
+    primary key(front_id)
+);
+INSERT INTO front_pages(title, text, photo_url, bg_color, text_color, title_color) 
+VALUES 
+("Por que adoptar?","Facere at pariatur, repellendus nulla, neque dolorem accusamus debitis quasi nostrum nihil perferendis quae quisquam dolorum corporis cumque temporibus quaerat placeat rerum.","https://firebasestorage.googleapis.com/v0/b/juandfe.appspot.com/o/api%2Fv1%2Fpets%2Fdogs%2Fperros1024x1024.jpg?alt=media&token=e629ebfb-f72a-4d8f-b01c-98a989eb4259","#223","#fff","#fff");
