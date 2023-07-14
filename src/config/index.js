f2b4053006aceecf05a99fe28bb1912ed3cece1c
JuanDNJ <juandevnjv@gmail.com>
2023-07-14 05:30:21 +0200

    delete

--- src/config/index.js
+++ src/config/index.js
@@ -4,6 +4,6 @@ config()
 export const PORT_SERVER = process.env.PORT_SERVER || 5174;
 export const DB_PORT = process.env.DB_PORT || 9500;
 export const DB_HOST = process.env.DB_HOST || "localhost";
-export const DB_USER = process.env.DB_USER || "juandfe";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "NereaJoel@2023";
-export const DB_NAME = process.env.DB_NAME || "mascotas";
+export const DB_USER = process.env.DB_USER || "";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "";
+export const DB_NAME = process.env.DB_NAME || "";
