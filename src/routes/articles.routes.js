63c1b53fb770f94cdd6b4015c7d3a742ec2a1415
JuanDNJ <juandevnjv@gmail.com>
2023-07-14 20:21:03 +0200

    Create: Ruta articulos

---
+++ src/routes/articles.routes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { articles } from "../controllers/articles.ctr.js";
+
+const router = Router();
+
+router.get('/articles', articles.all)
+router.get('/articles/search/:search', articles.search)
+router.post('/articles/article', articles.create)
+router.delete('/articles', articles.delete)
+router.put('/articles', articles.update)
+router.get('/articles/article/:article', articles.get)
+
+export default router;
